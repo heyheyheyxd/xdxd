620e9e15d0efc190d3a8150eb0aa8c698ca27584
Dmitry <usimovd@gmail.com>
2024-03-15 17:25:31 +0300

    add todo app

---
+++ src/components/Icon.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let name;
+    const icons = [
+        {
+            name: "check-mark",
+        },
+        {
+            name: "delete",
+        },
+    ];
+    let icon = icons.find((item) => item.name === name);
+</script>
