586d25d4f027e01efcf790d74127aab8e31b720f
Dmitry <usimovd@gmail.com>
2024-03-16 23:28:22 +0300

    fdsfds

---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,23 @@
+<h1>Контакты</h1>
+
+<div class="contact-info">
+    <h2>Наши контакты</h2>
+    <p>Телефон: +7 (902) 304-86-00</p>
+    <p>Email: purdy2121@yandex.ru</p>
+    <p>Адрес: ул. Примерная, 123, г. Примерный</p>
+</div>
+
+<div class="social-media">
+    <h2>Мы в социальных сетях</h2>
+    <ul>
+        <li><a href="https://vk.com/funeral_2">Вконтакте</a></li>
+        <li><a href="https://funpay.com/users/9185627/">Funpay</a></li>
+        <li><a href="https://www.instagram.com/_1nfinite_/?hl=ru">Instagram</a></li>
+    </ul>
+</div>
+
+<div class="working-hours">
+    <h2>Часы работы</h2>
+    <p>Понедельник - Пятница: 9:00 - 18:00</p>
+    <p>Суббота - Воскресенье: выходные</p>
+</div>
